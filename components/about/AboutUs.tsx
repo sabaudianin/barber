88bf56a09e9cd31decf36ea9b7e21c05a7f80446
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 <sabaudianin@gmail.com>
2025-12-16 15:08:47 +0100

    feat set mobile menu display

--- components/about/AboutUs.tsx
+++ components/about/AboutUs.tsx
@@ -9,7 +9,7 @@ export const AboutUs = () => {
           Welcome to Barber House Saska Kępa, a modern barbershop rooted in the
           classic craft of men&apos;s grooming.
         </h2>
-        <p className="font-bokor py-2 indent-2 text-zinc-400">
+        <p className="font-bokor py-2 indent-2 text-zinc-400 text-lg">
           Located in the heart of Warsaw&apos;s artistic Saska Kępa district, we
           blend skilled craftsmanship, a relaxed atmosphere, and a passion for
           precision. We specialize in clean fades, sharp cuts, and tailored
@@ -26,20 +26,45 @@ export const AboutUs = () => {
             className=" mx-auto sepia"
           />
         </div>
-        <ul className="p-2 list-disc list-inside  space-y-1 mt-8 tracking-[0.25em]  bg-stone-800 font-mono text-xs text-zinc-300">
-          <h2 className=" py-2 text-center font-lime text-xl text-white">
-            Why Choose Us?
-          </h2>
-          <li className="font-sans">
-            Skilled barbers with a passion for the craft
-          </li>
-          <li className="font-mono">
-            Expert beard shaping and grooming rituals
-          </li>
-          <li className="">Complimentary coffee and a relaxed vibe</li>
-          <li className="">Authentic Saska Kępa atmosphere</li>
-          <li className="">No rush - quality and detail first</li>
-        </ul>
+        <article className="grid lg:grid-cols-2 lg:items-stretch">
+          <ul className="p-2 list-inside  space-y-1 tracking-[0.25em] bg-stone-800 lg:h-full flex flex-col font-mono text-xs text-zinc-300 mt-auto">
+            <h2 className="py-2 text-center font-lime text-xl text-white">
+              Why Choose Us?
+            </h2>
+            <li className="font-sans">
+              <span className="text-xl">💈</span>Skilled barbers with a passion
+              for the craft
+            </li>
+            <li className="font-mono">
+              <span className="text-xl">💈</span>Expert beard shaping and
+              grooming rituals
+            </li>
+            <li className="">
+              {" "}
+              <span className="text-xl">💈</span>Complimentary coffee and a
+              relaxed vibe
+            </li>
+            <li className="">
+              {" "}
+              <span className="text-xl">💈</span>Authentic Saska Kępa atmosphere
+            </li>
+            <li className="">
+              {" "}
+              <span className="text-xl">💈</span>No rush - quality and detail
+              first
+            </li>
+          </ul>
+          <div className="relative h-80 p-2 lg:h-full">
+            <div className="w-full absolute inset-0 bg-[radial-gradient(circle_at_center,_rgba(0,0,0,0.4)_50%,_rgba(0,0,0,0.75)_90%,_rgba(0,0,0,0.99)_100%)] rounded z-10" />
+            <Image
+              src="/images/equipment.jpg"
+              fill
+              alt="Barber equipment"
+              className="object-cover rounded"
+              sizes="(min-width: 800px) 50vw, 100vw"
+            />
+          </div>
+        </article>
       </div>
     </section>
   );
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,8 +1,15 @@
 "use client";
+import Link from "next/link";
+import { useState } from "react";
 
 import { VisitButton } from "@/elements/visitButton/VisitButton";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdClose } from "react-icons/md";
 
 export const Navbar = () => {
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const toggleOpen = () => setMobileOpen((prev) => !prev);
+
   return (
     <section>
       <header className="relative z-20 flex items-center justify-between px-2 md:px-8 py-6 md:py-12 max-w-7xl mx-auto">
@@ -10,6 +17,11 @@ export const Navbar = () => {
           <p className="font-diplomata text-xl text-[var(--color-accent)] font-bold tracking-widest">
             Barber Shop
           </p>
+        </div>
+        <div>
+          <button onClick={toggleOpen}>
+            {mobileOpen ? <GiHamburgerMenu /> : <MdClose />}
+          </button>
         </div>
 
         <nav className="hidden md:flex gap-8 font-semibold uppercase tracking-[0.05em] text-zinc-300 font-lime  ">
@@ -27,6 +39,25 @@ export const Navbar = () => {
           </button>
           <VisitButton />
         </nav>
+
+        {mobileOpen && (
+          <nav className="flex flex-col absolute">
+            {" "}
+            <button className="hover:text-[var(--accent-light)] transition">
+              Usługi
+            </button>
+            <button className="hover:text-[var(--accent-light)] transition">
+              Cennik
+            </button>
+            <button className="hover:text-[var(--accent-light)] transition">
+              Galeria
+            </button>
+            <button className="hover:text-[var(--accent-light)] transition">
+              Kontakt
+            </button>
+            <VisitButton />
+          </nav>
+        )}
       </header>
     </section>
   );
