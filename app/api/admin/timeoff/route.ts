6680687c781266081d2547f3d14a14bb585611c9
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 <sabaudianin@gmail.com>
2026-02-02 14:28:47 +0100

    feat timoeff method POST and DLETE backend

--- app/api/admin/timeoff/route.ts
+++ app/api/admin/timeoff/route.ts
@@ -3,6 +3,7 @@ import { DateTime } from "luxon";
 import { prisma } from "@/lib/prisma";
 import { ZONE } from "@/lib/time/date";
 import { createTimeOffSchema } from "@/lib/schemas/timeoff";
+import { NEVER } from "zod";
 
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
@@ -47,4 +48,74 @@ export async function GET(req: Request) {
     },
   });
   return NextResponse.json({ date, barberId, items });
+}
+
+export async function POST(req: Request) {
+  const json = await req.json().catch(() => null);
+  if (!json) {
+    return NextResponse.json(
+      { error: "Invalid json body in Post" },
+      { status: 400 },
+    );
+  }
+
+  const parsedItem = createTimeOffSchema.safeParse(json);
+  if (!parsedItem.success) {
+    return NextResponse.json(
+      {
+        erro: "Parsing error",
+        details: parsedItem.error,
+      },
+      { status: 400 },
+    );
+  }
+
+  const { barberId, startAt, endAt, reason } = parsedItem.data;
+
+  //walidacja
+  const barber = await prisma.barber.findUnique({
+    where: { id: barberId },
+    select: { id: true, isActive: true },
+  });
+
+  if (!barber || !barber.isActive) {
+    return NextResponse.json(
+      { error: "Barber inactive or not found" },
+      { status: 404 },
+    );
+  }
+
+  //parsowanie dat
+  const start = DateTime.fromISO(startAt, { zone: ZONE });
+  const end = DateTime.fromISO(endAt, { zone: ZONE });
+
+  if (!start.isValid || !end.isValid) {
+    return NextResponse.json(
+      { error: "Invalid start or end use ISO dateTime" },
+      { status: 400 },
+    );
+  }
+  if (end <= start) {
+    return NextResponse.json(
+      { error: "End cannot be before start" },
+      { status: 400 },
+    );
+  }
+
+  const createdItem = await prisma.timeOff.create({
+    data: {
+      barberId,
+      startAt: start.toJSDate(),
+      endAt: end.toJSDate(),
+      reason: reason ? reason : null,
+    },
+    select: {
+      id: true,
+      startAt: true,
+      endAt: true,
+      reason: true,
+      barber: { select: { id: true, name: true } },
+    },
+  });
+  return NextResponse.json(createdItem, { status: 201 });
 }
